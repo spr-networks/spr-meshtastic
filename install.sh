ddba8b48ce386236a406a259d5b29ec68d05bde5
Alex Rad <alex@supernetworks.org>
2026-07-20 19:02:48 -0700

    Isolate krun plugin networking

--- install.sh
+++ install.sh
@@ -39,12 +39,12 @@ fi
 chmod 600 "$SUPERDIR/configs/plugins/spr-meshtastic/config.json"
 
 KRUN_MAC="02:53:50:52:4b:09"
-KRUN_TAP="kmeshtastic0"
+PLUGIN_INTERFACE="spr-meshtastic"
 curl --fail-with-body --silent --show-error "http://127.0.0.1/device?identity=${KRUN_MAC}" \
   -H "Authorization: Bearer ${SPR_API_TOKEN}" -H "Content-Type: application/json" \
   -X PUT --data-raw "{\"MAC\":\"${KRUN_MAC}\",\"Name\":\"spr-meshtastic\",\"Policies\":[\"lan\",\"dns\"],\"Groups\":[\"meshstatic\"]}" >/dev/null
-if ! sudo nft get element inet filter dhcp_access "{ \"${KRUN_TAP}\" . ${KRUN_MAC} }" >/dev/null 2>&1; then
-  sudo nft add element inet filter dhcp_access "{ \"${KRUN_TAP}\" . ${KRUN_MAC} : accept }"
+if ! sudo nft get element inet filter dhcp_access "{ \"${PLUGIN_INTERFACE}\" . ${KRUN_MAC} }" >/dev/null 2>&1; then
+  sudo nft add element inet filter dhcp_access "{ \"${PLUGIN_INTERFACE}\" . ${KRUN_MAC} : accept }"
 fi
 
 ./build_docker_compose.sh
@@ -64,6 +64,6 @@ API=127.0.0.1
 curl "http://${API}/firewall/custom_interface" \
 -H "Authorization: Bearer ${SPR_API_TOKEN}" \
 -X 'PUT' \
---data-raw "{\"SrcIP\":\"${CONTAINER_IP}\",\"Interface\":\"${KRUN_TAP}\",\"Policies\":[\"lan\",\"dns\"],\"Groups\":[\"meshstatic\"]}"
+--data-raw "{\"SrcIP\":\"${CONTAINER_IP}\",\"Interface\":\"${PLUGIN_INTERFACE}\",\"Policies\":[\"lan\",\"dns\"],\"Groups\":[\"meshstatic\"]}"
 
 docker compose -f docker-compose-kvm.yml restart
